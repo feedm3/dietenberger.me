d4b97f48da308c84a7a1cb0ac91c42c34acec9c0
Fabian Dietenberger <fabian.dietenberger@sinnerschrader.com>
2018-01-27 13:26:42 +0100

    add medium and put google analytics into script file

--- script/main.js
+++ script/main.js
@@ -4,6 +4,7 @@
 
 'use strict';
 
+// floating out elements on page scroll
 window.addEventListener('scroll', function () {
     var scrollTop = window.pageYOffset ? window.pageYOffset : document.documentElement.scrollTop;
 
@@ -16,3 +17,11 @@ window.addEventListener('scroll', function () {
 });
  
  
+// generic google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-64173212-2', 'auto');
+ga('send', 'pageview');
