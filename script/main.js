d9fa1da5d4a7aea59e007fa02da76c218868f9a5
Fabian Dietenberger <fabian.dietenberger@sinnerschrader.com>
2018-01-27 13:23:40 +0100

    remove jquery

--- script/main.js
+++ script/main.js
@@ -4,9 +4,15 @@
 
 'use strict';
 
-$(window).scroll(function(){
-    $(".fade").css("opacity", 1-
-        $(window).scrollTop() / 250);
+window.addEventListener('scroll', function () {
+    var scrollTop = window.pageYOffset ? window.pageYOffset : document.documentElement.scrollTop;
+
+    var fadingElements = document.getElementsByClassName("fade");
+    for (var i = 0; i<fadingElements.length; i++) {
+        var fadingElement = fadingElements[i];
+        console.log('set', 1 - scrollTop / 250);
+        fadingElement.style.opacity = 1 - scrollTop / 250;
+    }
 });
  
  
