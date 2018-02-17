47175bbe70a6bcdee19ee67bffb947eac9515ac1
Fabian Dietenberger <piano.fabian@t-online.de>
2018-02-17 15:48:40 +0100

    Improve styling

--- script/main.js
+++ script/main.js
@@ -11,7 +11,6 @@ window.addEventListener('scroll', function () {
     var fadingElements = document.getElementsByClassName("fade");
     for (var i = 0; i<fadingElements.length; i++) {
         var fadingElement = fadingElements[i];
-        console.log('set', 1 - scrollTop / 250);
         fadingElement.style.opacity = 1 - scrollTop / 250;
     }
 });
