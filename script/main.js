2f35d3b832462d5e8c8d3f5871b4e58352111766
Fabian Dietenberger <feedm3@users.noreply.github.com>
2021-04-10 20:05:09 +0200

    feat: remove google analytics

--- script/main.js
+++ script/main.js
@@ -23,13 +23,3 @@ window.addEventListener('scroll', function () {
         }
     }
 });
-
-/**
- * Generic google analytics stuff.
- */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-64173212-2', 'auto');
-ga('send', 'pageview');
